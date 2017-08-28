c79b6dc4f5a01bed5e39908ee12c0070bf5bf06e
Unknown <mruizsantaella@gmail.com>
2017-08-28 16:52:35 +0200

    Testing functions con JUnit

--- src/main/kotlin/com/mruiz84/hello/kotlin/DefiningFunctions.kt
+++ src/main/kotlin/com/mruiz84/hello/kotlin/DefiningFunctions.kt
@@ -1,12 +1,22 @@
 package com.mruiz84.hello.kotlin
 
+// visit https://kotlinlang.org/docs/reference/basic-syntax.html#defining-functions
 class DefiningFunctions {
 
     companion object Factory {
         fun create(): DefiningFunctions = DefiningFunctions()
     }
 
-    fun sum(a: Int, b: Int): Int {
+    // Function having two Int parameters with Int return type:
+    fun standardSum(a: Int, b: Int): Int {
         return a + b
+    }
+
+    // Function with an expression body and inferred return type:
+    fun expressionBodySum(a: Int, b: Int) = a + b
+
+    // Function returning no meaningful value: (Unit return type could be omitted)
+    fun printSum(a: Int, b: Int): Unit {
+        println("sum of $a and $b is ${a + b}")
     }
 }
--- src/test/kotlin/com/mruiz84/hello/kotlin/DefiningFunctionsTest.kt
+++ src/test/kotlin/com/mruiz84/hello/kotlin/DefiningFunctionsTest.kt
@@ -1,6 +1,7 @@
 package com.mruiz84.hello.kotlin
 
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
 import kotlin.test.assertEquals
 
 class DefiningFunctionsTest {
@@ -8,7 +9,18 @@ class DefiningFunctionsTest {
     var cut = DefiningFunctions.create()
 
     @Test
-    fun f() {
-        assertEquals(8, cut.sum(3, 5))
+    fun `sum 2 integer numbers with standardSum`() {
+        assertEquals(8, cut.standardSum(3, 5))
     }
+
+    @Test
+    fun `sum 2 integer numbers with `() {
+        assertEquals(8, cut.expressionBodySum(3, 5))
+    }
+
+    @Test
+    fun `print the sum for two operands`() {
+        cut.printSum(3, 5)
+    }
+
 }
